b2b62836f26e8c9975ecd96df50f476be218b548
StruzhakS <StruzhakS@gmail.com>
2023-08-16 10:38:31 +0300

    add mainPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import Layout from 'components/Layout/Layout';
 import Auth from 'pages/Auth';
 import HomePage from 'pages/HomePage';
+import MainPage from 'pages/MainPage';
 import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
@@ -10,6 +11,8 @@ export const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="/:auth" element={<Auth />} />
+          <Route path="/mainPage" element={<MainPage />} />
+
           <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
