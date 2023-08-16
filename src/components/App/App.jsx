383848b77a6532c131d3d26797c24c746bac42cb
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-08-16 19:26:25 +0300

    fix_AuthLayout

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,24 +8,21 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from '../../pages/Auth/SignIn/SignIn';
 import SignUp from '../../pages/Auth/SignUp/SignUp';
 import ForgotPass from '../../pages/Auth/ForgotPass/ForgotPass';
-import AuthLayout from '../Layout/Auth/Auth';
 
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/auth' element={<AuthLayout />}>
-          <Route path='forgot-password' element={<ForgotPass />} />
-          <Route path='signin' element={<SignIn />} />
-          <Route path='signup' element={<SignUp />} />
-          <Route path='' element={<MainAuth />} />
-        </Route>
-        <Route path='/' element={<Layout />}>
+        <Route path="/auth/forgot-password" element={<ForgotPass />} />
+        <Route path="/auth/signin" element={<SignIn />} />
+        <Route path="/auth/signup" element={<SignUp />} />
+        <Route path="/auth" element={<MainAuth />} />
+        <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path='/mainPage' element={<MainPage />} />
-          <Route path='/recomendedFood' element={<RecomendedFood />} />
-          <Route path='/diary' element={<Diary />} />
-          <Route path='*' element={<HomePage />} />
+          <Route path="/mainPage" element={<MainPage />} />
+          <Route path="/recomendedFood" element={<RecomendedFood />} />
+          <Route path="/diary" element={<Diary />} />
+          <Route path="*" element={<HomePage />} />
         </Route>
       </Routes>
     </>
