3235dc7cab8472142711d4602883531f8fb4ffe5
StruzhakS <StruzhakS@gmail.com>
2023-08-17 13:20:34 +0300

    add privateRoute

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,6 +7,9 @@ import { Route, Routes } from 'react-router-dom';
 import SignIn from '../../pages/Auth/SignIn/SignIn';
 import SignUp from '../../pages/Auth/SignUp/SignUp';
 import ForgotPass from '../../pages/Auth/ForgotPass/ForgotPass';
+import PublicRoute from 'containers/PublicRoute.jsx';
+import PrivateRoute from 'containers/PrivateRoute';
+import MainPage from 'pages/MainPage';
 
 export const App = () => {
   return (
@@ -14,12 +17,23 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<MainAuth />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route
+            path="/signin"
+            element={
+              <PublicRoute component={<SignIn />} redirect="/recomendedFood" />
+            }
+          />
+          {/* <Route path="/signin" element={<SignIn />} /> */}
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPass />} />
-          <Route path="/recomendedFood" element={<RecomendedFood />} />
+
+          <Route
+            path="/recomendedFood"
+            element={<PrivateRoute component={<RecomendedFood />} />}
+          />
+
           <Route path="/diary" element={<Diary />} />
-          <Route path="*" element={<HomePage />} />
+          <Route path="*" element={<MainAuth />} />
         </Route>
       </Routes>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import s from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import iconsSrc from '../../assets/icons/symbol-defs.svg';
 import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+import avatar from '../../assets/icons/emoji/Waight.png';
+import Modal from 'react-modal';
+import ReactModal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  overlay: {
+    background: 'transparent',
+  },
+};
+
+// const bg = {
+//   overlay: {
+//     background: 'transparent',
+//   },
+// };
+
+Modal.setAppElement('#root');
 
 const Header = () => {
-  return (
+  const isAuth = useSelector(state => state.auth.token);
+  const [setingsModalIsOpen, setSetingsModalIsOpen] = useState(false);
+
+  // ReactModal
+
+  return isAuth ? (
+    <div className={s.header}>
+      <NavLink to={'/'} className={s.logoLink}>
+        Your Health
+      </NavLink>
+      <div></div>
+      <div></div>
+      <button
+        type="button"
+        className={s.userBtn}
+        onClick={() => setSetingsModalIsOpen(true)}
+      >
+        UserName <img src={avatar} alt="" className={s.avatarImg} />
+        <svg style={{ fill: 'white' }} width="14" height="14">
+          <use href={`${iconsSrc}#arrow-down`} />
+        </svg>
+      </button>
+
+      <ReactModal
+        className={s.userSettingsModal}
+        isOpen={setingsModalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={() => setSetingsModalIsOpen(false)}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button>Settings</button>
+        <button>Log out</button>
+      </ReactModal>
+    </div>
+  ) : (
     <div className={s.header}>
       <NavLink to={'/'} className={s.logoLink}>
         HealthyHub
