a6ceb89c9209f7745c098e5634b1abcb003bb828
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-08-17 18:18:56 +0300

    fix_header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,8 @@ import s from './Header.module.css';
 import { NavLink } from 'react-router-dom';
 import iconsSrc from '../../assets/icons/symbol-defs.svg';
 import clsx from 'clsx';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../../redux/auth/authSlice';
 import avatar from '../../assets/icons/emoji/Waight.png';
 import Modal from 'react-modal';
 import ReactModal from 'react-modal';
@@ -22,77 +23,70 @@ const customStyles = {
   },
 };
 
-// const bg = {
-//   overlay: {
-//     background: 'transparent',
-//   },
-// };
-
 Modal.setAppElement('#root');
 
 const Header = () => {
+  const dispatch = useDispatch();
   const isAuth = useSelector(state => state.auth.token);
-  const [setingsModalIsOpen, setSetingsModalIsOpen] = useState(false);
+  const [settingsModalIsOpen, setSettingsModalIsOpen] = useState(false);
 
-  // ReactModal
+  const handleLogout = () => {
+    dispatch(logout());
+    // Дополнительно, выполните переход на страницу входа или другую страницу после логаута
+    // history.push('/signin'); // Если используете роутинг и имеете доступ к объекту history
+  };
 
-  return isAuth ? (
+  return (
     <div className={s.header}>
       <NavLink to={'/'} className={s.logoLink}>
-        Your Health
+        {isAuth ? 'Your Health' : 'HealthyHub'}
       </NavLink>
-      <div></div>
-      <div></div>
-      <button
-        type="button"
-        className={s.userBtn}
-        onClick={() => setSetingsModalIsOpen(true)}
-      >
-        UserName <img src={avatar} alt="" className={s.avatarImg} />
-        <svg style={{ fill: 'white' }} width="14" height="14">
-          <use href={`${iconsSrc}#arrow-down`} />
-        </svg>
-      </button>
-
-      <ReactModal
-        className={s.userSettingsModal}
-        isOpen={setingsModalIsOpen}
-        // onAfterOpen={afterOpenModal}
-        onRequestClose={() => setSetingsModalIsOpen(false)}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <button>Settings</button>
-        <button>Log out</button>
-      </ReactModal>
-    </div>
-  ) : (
-    <div className={s.header}>
-      <NavLink to={'/'} className={s.logoLink}>
-        HealthyHub
-      </NavLink>
-      <nav className={s.navlink}>
-        <NavLink
-          to={'/signin'}
-          className={({ isActive }) =>
-            isActive ? clsx(s.authLinkUp, s.active) : s.authLinkUp
-          }
-        >
-          Sign in
-        </NavLink>
-        <span>/</span>
-        <NavLink
-          to={'/signup'}
-          className={({ isActive }) =>
-            isActive ? clsx(s.authLinkIn, s.active) : s.authLinkUp
-          }
-        >
-          Sign up
-        </NavLink>
-        <svg style={{ fill: 'white' }} width="24" height="24">
-          <use href={`${iconsSrc}#profile-circle`} />
-        </svg>
-      </nav>
+      {isAuth ? (
+        <div>
+          <button
+            type="button"
+            className={s.userBtn}
+            onClick={() => setSettingsModalIsOpen(true)}
+          >
+            UserName <img src={avatar} alt="" className={s.avatarImg} />
+            <svg style={{ fill: 'white' }} width="14" height="14">
+              <use href={`${iconsSrc}#arrow-down`} />
+            </svg>
+          </button>
+          <ReactModal
+            className={s.userSettingsModal}
+            isOpen={settingsModalIsOpen}
+            onRequestClose={() => setSettingsModalIsOpen(false)}
+            style={customStyles}
+            contentLabel="User Settings Modal"
+          >
+            <button onClick={handleLogout}>Log out</button>
+          </ReactModal>
+        </div>
+      ) : (
+        <nav className={s.navlink}>
+          <NavLink
+            to={'/signin'}
+            className={({ isActive }) =>
+              isActive ? clsx(s.authLinkUp, s.active) : s.authLinkUp
+            }
+          >
+            Sign in
+          </NavLink>
+          <span>/</span>
+          <NavLink
+            to={'/signup'}
+            className={({ isActive }) =>
+              isActive ? clsx(s.authLinkIn, s.active) : s.authLinkUp
+            }
+          >
+            Sign up
+          </NavLink>
+          <svg style={{ fill: 'white' }} width="24" height="24">
+            <use href={`${iconsSrc}#profile-circle`} />
+          </svg>
+        </nav>
+      )}
     </div>
   );
 };
