58eed4ae3861b41f098a393f35f856f1177df02c
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-17 18:51:26 +0200

    scroll

--- src/components/Dashboard/DashboarLineChart.jsx
+++ src/components/Dashboard/DashboarLineChart.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+ import React from 'react';
 import ReactECharts from 'echarts-for-react';
-import { useMediaQuery } from 'react-responsive';
+
+import css from './DashboardLineChart.module.css';
 
 const LineChart = () => {
   const values = [
-    2.0, 2.5, 1.6, 1.3, 1.4, 1.6, 1.9, 2.1, 2.5, 2.8, 2.1, 1.2, 1.5, 1.7,
+    1.0, 1.5, 1.6, 1.3, 1.4, 1.6, 1.2, 1.1, 1.5, 1.8, 1.1, 1.2, 1.5, 1.7, 1.0,
+    1.5, 1.6, 1.3, 1.4, 1.6, 1.2, 1.1, 1.5, 1.8, 1.1, 1.2, 1.5, 1.7, 1.2, 1.5,
+    1.7,
   ];
   const maxValue = Math.max(...values);
   const maxIndex = values.indexOf(maxValue) + 1;
-  const isMobile = useMediaQuery({ maxWidth: 320 });
+  const chartWidth = '715px';
+  const chartHeight = '400px';
+
+  const chartContainerStyle = {
+    width: chartWidth,
+    height: chartHeight,
+    position: 'relative',
+    overflow: 'hidden', // Відключаємо прокрутку на контейнері
+  };
+
+  const chartScrollContainerStyle = {
+    width: chartWidth,
+    height: chartHeight,
+    overflowY: 'scroll', // Увімкнути прокрутку для графіка
+  };
+
   const data = {
     xAxis: {
       type: 'category',
@@ -18,8 +36,16 @@ const LineChart = () => {
       ],
       axisLine: {
         lineStyle: {
-          color: 'rgba(182, 182, 182, 1)',
+          color: '#292928',
+          width: 0.5,
+          type: 'solid',
         },
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        color: '#B6B6B6',
       },
       splitLine: {
         show: true,
@@ -28,8 +54,7 @@ const LineChart = () => {
         },
       },
     },
-      yAxis: {
-        responsive: false,
+    yAxis: {
       type: 'value',
       color: '#B6B6B6',
       axisLabel: {
@@ -59,116 +84,56 @@ const LineChart = () => {
         type: 'line',
         symbol: 'circle',
         itemStyle: {
-          color:'rgba(227, 255, 168, 1)',
+          color: 'rgba(227, 255, 168, 1)',
+          width: 1,
         },
         symbolSize: (value, params) =>
           params.dataIndex + 1 === maxIndex ? 10 : 0,
         smooth: true,
         lineStyle: {
-        color: 'rgba(227, 255, 168, 1)', // Оновлений колір лінії
-      },
-
-        emphasis: {
-        itemStyle: {
           color: 'rgba(227, 255, 168, 1)',
         },
-      },
-         markPoint: {
-        data: [{ type: 'max', name: 'Max Value' }],
-        symbol: 'rect', // Змінено на 'rect' для квадрата
-        symbolSize: [104, 92], // Розміри квадрата
-        symbolOffset: [-1, '-55%'], // Зміщення, щоб символ був зверху середини маркера
 
-        itemStyle: {
-          color: '#0F0F0F', // Колір квадрата
-          borderRadius: 8, // Радіус кутів
-          shadowBlur: 14, // Розмиття тіні
-          shadowColor: 'rgba(227, 255, 168, 0.20)', // Колір тіні
+        emphasis: {
+          itemStyle: {
+            color: 'rgba(227, 255, 168, 1)',
+          },
         },
-      },
+        markPoint: {
+          data: [
+            {
+              type: 'max',
+              name: 'Max Value',
+              value: maxValue,
+              formatter: 'Max Value: {value|0.0a}',
+            },
+          ],
+          symbol: 'rect',
+          symbolSize: [98, 70],
+          symbolOffset: [50, '-60%'],
+
+            
+          itemStyle: {
+            color: '#0F0F0F',
+            borderRadius: 12,
+            shadowBlur: 14,
+            shadowColor: 'rgba(227, 255, 168, 0.20)',
+          },
+        },
       },
     ],
   };
-
-  return (
-    <div
-      style={{
-        width: '715px',
-        maxWidth: isMobile ? '100%' : 'none',
-        height: '40%',
-        overflowX: isMobile ? 'scroll' : 'hidden',
-      }}
-    >
-      <ReactECharts option={data} />
+  
+ 
+  return  (
+   <div className={css.lineChartContainer}>
+      <div style={chartContainerStyle}>
+        <div style={chartScrollContainerStyle}>
+          <ReactECharts option={data} opts={{ width: chartWidth, height: chartHeight }} />
+        </div>
+      </div>
     </div>
   );
 };
 
 export default LineChart;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useState } from 'react';
-
-// const LineChart = () => {
-//   const data = [10, 25, 15, 30, 20];
-//   const maxY = Math.max(...data);
-//   const height = 300;
-
-//   const points = data.map((value, index) => {
-//     const x = index * 50;
-//     const y = height - (value / maxY) * height;
-//     return `${x},${y}`;
-//   });
-
-//   const polylinePoints = points.join(' ');
-
-//   return (
-//     <svg width="400" height={height + 20}>
-//       {/* Вертикальні лінії на осі X */}
-//       {data.map((value, index) => (
-//         <line
-//           key={`vertical-${index}`}
-//           x1={index * 50}
-//           y1={0}
-//           x2={index * 50}
-//           y2={height}
-//           stroke="gray"
-//           strokeWidth="1"
-//         />
-//       ))}
-
-//       {/* Горизонтальні лінії на осі Y */}
-//       {Array.from({ length: 6 }).map((_, index) => (
-//         <line
-//           key={`horizontal-${index}`}
-//           x1={0}
-//           y1={height - (index * height) / 5}
-//           x2={400}
-//           y2={height - (index * height) / 5}
-//           stroke="gray"
-//           strokeWidth="1"
-//         />
-//       ))}
-
-//       {/* Лінія графіка */}
-//       <polyline points={polylinePoints} fill="none" stroke="blue" strokeWidth="2" />
-//     </svg>
-//   );
-// };
-
-// export default LineChart;
