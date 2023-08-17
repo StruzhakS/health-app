5fa25aea3b7bf4d475d30e4bd0c6a5b8a29f45de
Stark561 <123174593+Stark561@users.noreply.github.com>
2023-08-17 13:14:59 +0300

    gsg

--- src/components/RecomendedFood/RecomendedFood.jsx
+++ src/components/RecomendedFood/RecomendedFood.jsx
@@ -2,10 +2,31 @@ import React from 'react';
 import s from './RecomendedFood.module.css';
 import recomendedFood from '../../recomended-food.json';
 
+
 const RecomendedFood = () => {
   console.log(recomendedFood);
 
-  return <div className={s.Box}>RecomendedFood</div>;
+  return (
+    <section className={s.recomendedFoodSection}>
+      <h2 className={s.recomendedFoodTitle}>RecomendedFood</h2>
+      <div className={s.recomendedFoodBox}>
+        <img src="" alt="Food"/>
+        {/* <ul className={s.recomendedFoodList}>
+        {randomArr().map(({ name, amount, calories }) => (
+          <li className={s.recomendedFoodItem}Food key={name}>
+            <img src="" alt={name} />
+            <div>
+              <p>{name}</p>
+              <p>
+                {amount} <span>{`${calories} calories`}</span>
+              </p>
+            </div>
+          </li>
+        ))}
+        </ul> */}
+      </div>
+    </section>
+    )
 };
 
 export default RecomendedFood;
