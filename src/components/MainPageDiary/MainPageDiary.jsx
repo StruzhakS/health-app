8f31c02d53babc663f2a00f343a8e2a1e5a55077
MishaHudz <misha.hudz@gmail.com>
2023-08-18 11:40:15 +0300

    add mobile and tablet styles

--- src/components/MainPageDiary/MainPageDiary.jsx
+++ src/components/MainPageDiary/MainPageDiary.jsx
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom';
 import css from './MainPageDiary.module.css';
 
+import Icons from '../../assets/icons/symbol-defs.svg';
+import Breakfast from '../../assets/img/mobile/Breakfast.png';
+import Lunch from '../../assets/img/mobile/Lunch.png';
+import Dinner from '../../assets/img/mobile/Dinner.png';
+import Snack from '../../assets/img/mobile/Snack.png';
+
 const Data = [
   { carbonohidrates: 60, protein: 40, fat: 20 },
   { carbonohidrates: 110, protein: 79.5, fat: 24.8 },
@@ -18,12 +24,25 @@ const MainPageDiary = () => {
         </Link>
       </div>
 
-      <ul>
+      <ul className={css.mealList}>
         {Data.map(({ carbonohidrates, protein, fat }, i) => (
-          <li key={i}>
-            <div>
-              <img src="" alt="meal" />
-              <h3>
+          <li className={css.mealListItem} key={i}>
+            <div className={css.mealTitle}>
+              <img
+                width="36px"
+                height="36px"
+                src={
+                  i === 0
+                    ? Breakfast
+                    : i === 1
+                    ? Lunch
+                    : i === 2
+                    ? Dinner
+                    : Snack
+                }
+                alt="meal"
+              />
+              <h3 className={css.mealListItemTitle}>
                 {i === 0
                   ? 'Breakfast'
                   : i === 1
@@ -32,30 +51,43 @@ const MainPageDiary = () => {
                   ? 'Dinner'
                   : 'Snack'}
               </h3>
-              {carbonohidrates && (
-                <ul>
-                  <li>
-                    <p>
-                      Carbonohidrates:
-                      <span>{carbonohidrates}</span>
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Protein:
-                      <span>{protein}</span>
-                    </p>
-                  </li>
-                  <li>
-                    <p>
-                      Fat:
-                      <span>{fat}</span>
-                    </p>
-                  </li>
-                </ul>
-              )}
-              {!carbonohidrates && <button>Test</button>}
             </div>
+            {carbonohidrates && (
+              <ul className={css.mealAdditionalInfoList}>
+                <li className={css.mealAdditionalInfoListItem}>
+                  <p className={css.mealAdditionalInfoDescription}>
+                    Carbonohidrates:
+                    <span className={css.mealAdditionalInfoValue}>
+                      &nbsp;{carbonohidrates}
+                    </span>
+                  </p>
+                </li>
+                <li className={css.mealAdditionalInfoListItem}>
+                  <p className={css.mealAdditionalInfoDescription}>
+                    Protein:
+                    <span className={css.mealAdditionalInfoValue}>
+                      &nbsp;{protein}
+                    </span>
+                  </p>
+                </li>
+                <li className={css.mealAdditionalInfoListItem}>
+                  <p className={css.mealAdditionalInfoDescription}>
+                    Fat:
+                    <span className={css.mealAdditionalInfoValue}>
+                      &nbsp;{fat}
+                    </span>
+                  </p>
+                </li>
+              </ul>
+            )}
+            {!carbonohidrates && (
+              <button className={css.recordMealButton}>
+                <svg width="16px" height="16px" className={css.recordMealIcon}>
+                  <use xlinkHref={`${Icons}#add`} />
+                </svg>
+                Record your meal
+              </button>
+            )}
           </li>
         ))}
       </ul>
