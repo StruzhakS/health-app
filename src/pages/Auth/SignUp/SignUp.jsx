95399bd4a82e941ba7855c49ba3d61ee111b6b15
GIF1985 <125868361+GIF1985@users.noreply.github.com>
2023-08-19 16:48:44 +0300

    add_fix_Password_Hide_Show

--- src/pages/Auth/SignUp/SignUp.jsx
+++ src/pages/Auth/SignUp/SignUp.jsx
@@ -8,6 +8,8 @@ import { updateAuthUser } from '../../../redux/auth/authSlice';
 import IllustrationDesktop from '../../../assets/img/desktop/Illustration.png';
 import IllustrationTablet from '../../../assets/img/tablet/Illustration.png';
 import IllustrationMobile from '../../../assets/img/mobile/Illustration.png';
+import EyeIcon from '../../../assets/icons/Illustration/eye.svg';
+import EyeOffIcon from '../../../assets/icons/Illustration/eye-off.svg';
 
 const SignUp = () => {
   const dispatch = useDispatch();
@@ -110,13 +112,19 @@ const SignUp = () => {
                 value={password}
                 onChange={e => setPassword(e.target.value)}
               />
-              <button
-                type="button"
-                className={styles.showPasswordButton}
-                onClick={toggleShowPassword}
-              >
-                {showPassword ? 'Hide' : 'Show'}
-              </button>
+              <div className={styles.passwordInputIcon}>
+                <button
+                  type="button"
+                  className={styles.showPasswordButton}
+                  onClick={toggleShowPassword}
+                >
+                  {showPassword ? (
+                    <img src={EyeOffIcon} alt="Hide" width="16" height="16" />
+                  ) : (
+                    <img src={EyeIcon} alt="Show" width="16" height="16" />
+                  )}
+                </button>
+              </div>
               {password && (
                 <p
                   className={`${styles.passwordMessage} ${
