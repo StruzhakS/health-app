abb36d88ba16cd5c26c95921463f90928ad76d11
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-08-21 21:11:06 +0200

    change back

--- src/components/DashboardLastMonth/DateSelect/DateSelect.jsx
+++ src/components/DashboardLastMonth/DateSelect/DateSelect.jsx
@@ -1,34 +1,40 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal'; // Імпорт бібліотеки react-modal
 import sprite from '../../../assets/icons/symbol-defs.svg';
 import s from './DateSelect.module.css';
 
 const DateSelector = () => {
-  const [selectedOption, setSelectedOption] = useState('lastMonth');
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
   return (
-    <div className={s.selectBox}>
-      <select
-        className={s.selectBox}
-        value={selectedOption}
-        onChange={handleOptionChange}
+    <div className={s.dateSelectorContainer}>
+      <button onClick={handleModalOpen} className={s.lastMonthBtn} >Last Month
+        <svg className={s.dashboardArrowDownSvg}>
+          <use
+            className={s.dashboardArrowDownSvgLink}
+            xlinkHref={`${sprite}#arrow-down`}
+          ></use>
+        </svg>
+      </button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={handleModalClose}
+        contentLabel="ModalDashBoard"
+        className={s.dashboardSelect} 
+        overlayClassName={s.modalOverlay} 
       >
-        <option className={s.lastYearBtn} value="lastMonth">Last Month</option>
-        <option   className={s.lastYearBtn} value="lastYear">Last Year</option>
-      </select>
+        <p onClick={handleModalClose}>Last Year</p>
+      </Modal>
     </div>
   );
 };
 
 export default DateSelector;
-
-
-// <div class="select_box">
-//  <select>
-//    <option>Test This Select</option>
-//    <option>Test This Select</option>
-//  </select>
-// </div>
